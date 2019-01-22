b648e19b393fe9e843357bc32223aa38a1115a38
Greyson Malo <greyson@mail.com>
2019-01-21 19:19:40 -0800

    added new graphql yoga server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
-import myAddFunction, { subtract } from "./math.js";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log(myaddFunction(1, 10));
-console.log(subtract(20, 10));
+//Type definitions
+
+const typeDefs = `
+
+type Query {
+   name: String!,
+   lastname: String!,
+}
+
+`;
+//Resolvers
+
+const resolvers = {
+  Query: {
+    firstname() {
+      return "This is where your first name would go";
+    },
+    lastname() {
+      return "Your last name would go here";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+console.log("Testing server");
+
+server.start(() => {
+  console.log("The server has started");
+});
