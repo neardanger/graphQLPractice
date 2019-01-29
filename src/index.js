ff5060ccb8dcaaa6e9285860b7c42c72010be798
Greyson Malo <greyson@mail.com>
2019-01-28 17:38:17 -0800

    added posts

--- src/index.js
+++ src/index.js
@@ -1,35 +1,60 @@
 import { GraphQLServer } from "graphql-yoga";
-import { getPackedSettings } from "http2";
 
 //Type definitions
 
 //example
-const typeDefs = `
 
-type Query {
-   id: ID!
-   firstname: String!,
-   lastname: String!,
-   age: Int!,
-   gpa: Float!
+//Parenthesis (parent, args, ctx, info)
+const typeDefs = `
+  type Query {
+    greeting(name: String, position:String): String!
+    add(a: Float!, b: Float!): Float!
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
 }
+
+  type Post {
+    id: ID!,
+    title: String!,
+    body: String!,
+    published: Boolean!
+  }
 
 `;
 //Resolvers
 
 const resolvers = {
   Query: {
-    firstname() {
-      return "This is where your first name would go";
+    greeting(parent, args, ctx, info) {
+      console.log(parent, args, ctx, info);
+      if (args.name && args.position) {
+        return `Hello, this is a greeting, ${args.name} and your position is ${
+          args.position
+        }`;
+      }
     },
-    lastname() {
-      return "Your last name would go here";
+    me() {
+      return {
+        id: 1234,
+        name: "Steven Magadan",
+        email: "steven.magadan@hotmail.com",
+        age: 18
+      };
     },
-    age() {
-      return 17;
-    },
-    gpa() {
-      return 3.1;
+    post() {
+      return {
+        id: 9222,
+        title: "To kill a mocking bird",
+        body: "This is how you kill a mocking bird",
+        published: true
+      };
     }
   }
 };
