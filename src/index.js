379e21cb9ecd0837ea46b6deb67ca8f547a65ac8
Greyson Malo <steven.magadan@hotmail.com>
2019-03-29 14:10:38 -0700

    added more content

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import AddArgumentsAsVariablesTransform from "graphql-tools/dist/transforms/AddA
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
-import Subscription from "./resovlers/Subscription";
+import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
+
+const pubsub = new PubSub();
 
 //Type definitions
 const server = new GraphQLServer({
@@ -20,7 +22,8 @@ const server = new GraphQLServer({
     Comment
   },
   context: {
-    db
+    db,
+    pubsub
   }
 });
 
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,7 +1,29 @@
 const Subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0;
 
+      setInterval(() => {
+        count++;
+        pubsub.publish("count", {
+          count: count
+        });
+      }, 1000);
 
-    
+      return pubsub.asyncIterator("count");
+    }
+  },
+  comment: {
+    subscribe(parent, { postId }, { db }, info) {
+      const post = db.posts.find(post => post.id === postId && post.published);
+      if (!post) {
+        throw new Error("That post does not exist in the database.");
+      }
+      return pubsub.asyncIterator(`comment ${postId}`);
+    }
+  }
 };
 
 export { Subscription as default };
+
+//subscription js
